1e53da1f2255cb4cf0c90f5b424da0dbbb177884
kenphung <quangthaiphung2911200@gmail.com>
2025-08-02 23:19:43 +0700

    update command prom

--- netem.py
+++ netem.py
@@ -20,61 +20,99 @@ class Config:
 
 def unset(config: Config):
     os.system(f"sudo tc qdisc del dev {config.interface} root")
-    print(f"reset config for {config.interface}")
+    print(f"sudo tc qdisc del dev {config.interface} root")
 
 
 def add_prio_qdisc(config: Config):
     print(f"--- Add Prio qdisc ---")
     print(f"There are {len(config.hosts)} hosts")
     os.system(f"sudo tc qdisc add dev {config.interface} root handle 1: prio")
+    print(f"sudo tc qdisc add dev {config.interface} root handle 1: prio")
     os.system(
+        f"sudo tc qdisc replace dev {config.interface} root handle 1: prio bands {len(config.hosts) + 1}"
+    )
+    print(
         f"sudo tc qdisc replace dev {config.interface} root handle 1: prio bands {len(config.hosts) + 1}"
     )
 
 
 def config_full(config: Config):
-    unique_id = 2
+    unique_id = 3
     for index, host in enumerate(config.hosts):
         os.system(
-            f"sudo tc qdisc add dev {config.interface} parent 1:{index+1} handle {unique_id}: htb"
+            f"sudo tc qdisc add dev {config.interface} parent 1:{index+2} handle {unique_id}: htb"
+        )
+        print(
+            f"sudo tc qdisc add dev {config.interface} parent 1:{index+2} handle {unique_id}: htb"
         )
         os.system(
             f"sudo tc class add dev {config.interface} parent {unique_id}: classid {unique_id}:1 htb rate {host.bandwidth}kbit"
         )
-        os.system(
-            f"sudo tc qdisc add dev {config.interface} parent {unique_id}:1 handle {unique_id+1}: netem delay {host.latency}ms {host.jitter}ms"
+        print(
+            f"sudo tc class add dev {config.interface} parent {unique_id}: classid {unique_id}:1 htb rate {host.bandwidth}kbit"
         )
         os.system(
-            f"sudo tc filter add dev {config.interface} protocol ip parent 1: prio {index+1} u32 match ip dst {host.ip_address}/32 flowid 1:{index+1}"
+            f"sudo tc qdisc add dev {config.interface} parent {unique_id}:1 handle {unique_id+len(config.hosts)}: netem delay {host.latency}ms {host.jitter}ms"
+        )
+        print(
+            f"sudo tc qdisc add dev {config.interface} parent {unique_id}:1 handle {unique_id+len(config.hosts)}: netem delay {host.latency}ms {host.jitter}ms"
+        )
+        os.system(
+            f"sudo tc filter add dev {config.interface} protocol ip parent 1: prio {index+2} u32 match ip dst {host.ip_address}/32 flowid 1:{index+2}"
+        )
+        print(
+            f"sudo tc filter add dev {config.interface} protocol ip parent 1: prio {index+2} u32 match ip dst {host.ip_address}/32 flowid 1:{index+2}"
         )
         os.system(
             f"sudo tc filter add dev {config.interface} protocol ip parent {unique_id}: u32 match ip dst {host.ip_address}/32 flowid {unique_id}:1"
         )
-        unique_id += 2
+        print(
+            f"sudo tc filter add dev {config.interface} protocol ip parent {unique_id}: u32 match ip dst {host.ip_address}/32 flowid {unique_id}:1"
+        )
+
+        os.system(
+            f"sudo tc filter add dev {config.interface} protocol ip parent 1: prio {index+2} u32 match ip src {host.ip_address}/32 flowid 1:{index+2}"
+        )
+        print(
+            f"sudo tc filter add dev {config.interface} protocol ip parent 1: prio {index+2} u32 match ip src {host.ip_address}/32 flowid 1:{index+2}"
+        )
+        os.system(
+            f"sudo tc filter add dev {config.interface} protocol ip parent {unique_id}: u32 match ip src {host.ip_address}/32 flowid {unique_id}:1"
+        )
+        print(
+            f"sudo tc filter add dev {config.interface} protocol ip parent {unique_id}: u32 match ip src {host.ip_address}/32 flowid {unique_id}:1"
+        )
+        unique_id += 1
 
 
-# def static_delay(host: Host):
-#     os.system(f"sudo tc qdisc add dev {host.interface} root handle 1: prio")
-#     os.system(
-#         f"sudo tc qdisc add dev {host.interface} parent 1:1 handle 10: netem delay {host.latency}ms"
-#     )
-#     os.system(
-#         f"sudo tc filter add dev {host.interface} protocol ip parent 1:0 prio 1 u32 match ip dst {host.ip_address}/32 flowid 1:1"
-#     )
-#     print(f"Latency to {host.ip_address} set to {host.latency}ms")
+def config_Prometheus(config: Config):
+    os.system(f"sudo tc qdisc add dev {config.interface} parent 1:1 handle 2: htb")
+    print(f"sudo tc qdisc add dev {config.interface} parent 1:1 handle 2: htb")
+    os.system(
+        f"sudo tc filter add dev {config.interface} protocol ip parent 1: prio 1 u32 match ip sport 9090 0xffff flowid 1:1"
+    )
+    print(
+        f"sudo tc filter add dev {config.interface} protocol ip parent 1: prio 1 u32 match ip sport 9090 0xffff flowid 1:1"
+    )
+    os.system(
+        f"sudo tc filter add dev {config.interface} protocol ip parent 1: prio 1 u32 match ip sport 9100 0xffff flowid 1:1"
+    )
+    print(
+        f"sudo tc filter add dev {config.interface} protocol ip parent 1: prio 1 u32 match ip sport 9100 0xffff flowid 1:1"
+    )
+    os.system(
+        f"sudo tc filter add dev {config.interface} protocol ip parent 1: prio 1 u32 match ip dport 9090 0xffff flowid 1:1"
+    )
+    print(
+        f"sudo tc filter add dev {config.interface} protocol ip parent 1: prio 1 u32 match ip dport 9090 0xffff flowid 1:1"
+    )
+    os.system(
+        f"sudo tc filter add dev {config.interface} protocol ip parent 1: prio 1 u32 match ip dport 9100 0xffff flowid 1:1"
+    )
+    print(
+        f"sudo tc filter add dev {config.interface} protocol ip parent 1: prio 1 u32 match ip dport 9100 0xffff flowid 1:1"
+    )
 
-
-# def jitter_delay(host: Host):
-#     os.system(f"sudo tc qdisc add dev {host.interface} root handle 1: prio")
-#     os.system(
-#         f"sudo tc qdisc add dev {host.interface} parent 1:1 handle 10: netem delay {host.latency}ms {host.jitter}ms"
-#     )
-#     os.system(
-#         f"sudo tc filter add dev {host.interface} protocol ip parent 1:0 prio 1 u32 match ip dst {host.ip_address}/32 flowid 1:1"
-#     )
-#     print(
-#         f"Latency to {host.ip_address} set to {host.latency}ms with jitter {host.jitter}ms"
-#     )
 
 if __name__ == "__main__":
     # Load config file
@@ -102,11 +140,12 @@ if __name__ == "__main__":
     if len(args) >= 2:
         if args[1] == "reset":
             for host in hosts:
-                unset(host)
+                unset(config)
             sys.exit(0)
         else:
             sys.exit(0)
 
     unset(config)
     add_prio_qdisc(config)
+    config_Prometheus(config)
     config_full(config)
